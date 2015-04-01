8a1966e2e9ded4742616f5e5635ebb9ad7f5d09f
sereprz <sereprz@gmail.com>
2015-04-01 15:15:16 -0400

    refactoring and make km input

--- docClustering.py
+++ docClustering.py
@@ -2,6 +2,7 @@ from __future__ import division
 from sklearn.cluster import KMeans
 from time import time
 
+import collections as c
 import glob
 import nltk
 import math
@@ -16,68 +17,53 @@ path = '../../Datastore/Shakespeare/Normalized/*.txt'
 documents = {}
 
 for filename in glob.glob(path):
-	with open(filename, 'r') as f:
-		text = f.read()
-		key = re.sub( r'/', '', str(re.findall(r'/[a-z]+', filename)[0]))
-		documents[key] = nltk.word_tokenize(text)
-
-
-def tf(w, d):
-	return(d.count(w))
-
-def idf(w, D):
-	# inverse document frequency
-	return( math.log(len(D)/sum([1 for key in D.keys() if w in D[key]])))
-
-def tfidf(w, d, D):
-	# term freqency-inverse ddocument frequency
-	# http://en.wikipedia.org/wiki/Tf%E2%80%93idf
-	return(tf(w,d)*idf(w,D))
+    with open(filename, 'r') as f:
+        text = f.read()
+        key = re.sub( r'/', '', str(re.findall(r'/[a-z]+', filename)[0]))
+        documents[key] = nltk.word_tokenize(text)
 
 def cosine_similarity(d1, d2):
-	# cosine similarity between two vectors
-	return(np.dot(d1, d2)/(np.linalg.norm(d1)*np.linalg.norm(d2)))
-
-
+    # cosine similarity between two vectors
+    return(np.dot(d1, d2)/(np.linalg.norm(d1)*np.linalg.norm(d2)))
 
 ############################
 print 'calculating tf-idf weights'
 t0 = time()
 
-idf_counts = {}
+idf_counts = c.defaultdict(int)
 raw_freq = {}
 
-for key in documents.keys():
-	seen_in_doc = {}
-	for w in documents[key]:
-		if not seen_in_doc.get(w, False):
-			idf_counts[w] = idf_counts.get(w, 0) + 1
-			seen_in_doc[w] = True
+for title, words in documents.iteritems():
+    was_word_seen = c.defaultdict(bool)
+    for word in words:
+        if not was_word_seen[word]:
+            idf_counts[word] += 1
+            was_word_seen[word] = True
 
-for key in documents.keys():
-	raw_freq[key] = {}
-	for w in idf_counts:
-		raw_freq[key][w] = tf(w, documents[key])
+for title in documents.keys():
+    raw_freq[title] = {}
+    for word in idf_counts:
+        raw_freq[title][word] = documents[key].count(word)
 
 idf_weights = {}
-for w in idf_counts.keys():
-	idf_weights[w] = math.log(len(documents)/idf_counts[w])
+for word in idf_counts.keys():
+    idf_weights[word] = math.log(len(documents)/idf_counts[word])
 
 weights = {}
-for key in raw_freq.keys():
-	weights[key] = {}
-	for w in raw_freq[key]:
-		weights[key][w] = raw_freq[key][w]*idf_weights[w]
+for title in raw_freq.keys():
+    weights[title] = {}
+    for word in raw_freq[title]:
+        weights[title][word] = raw_freq[title][word]*idf_weights[word]
+
+unique_words_sorted = np.array(sorted(idf_counts.keys()))
+titles = np.array(sorted(documents.keys()))
+
+weights_matrix = []
+for title in titles:
+    this_play_weights = weights[title]
+    row = [this_play_weights[word] for word in unique_words_sorted]
+    weights_matrix.append(row)
+
+weights_matrix = np.matrix(weights_matrix)
 
 print "..done in %f s" % (time() - t0)
-
-## just testing
-#for key in weights.keys():
-#	print key, 1/cosine_similarity(weights['tamingoftheshrew'], weights[key])
-
-#df = pd.DataFrame(weights).T
-
-#km = KMeans(init='random', n_clusters = 2, n_init = 1, verbose = 0)
-#km.fit(df)
-
-#print km.labels_
